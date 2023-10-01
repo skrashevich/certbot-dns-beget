9f9ad168065e6385fa49a9fe48a208b3641701bc
Sergey Krashevich <svk@svk.su>
2023-10-01 13:40:00 +0300

    Update README and setup for Bunny.net DNS Authenticator Plugin
    This commit updates the README file to reflect the change from Beget DNS Authenticator plugin to Bunny.net DNS Authenticator Plugin for Certbot. It also adds badges for license, PyPI version, supported Python versions, documentation, and Snap Store.
    
    The setup.py file is updated to reflect the new version, author, author email, and Python requirements. The URL is also updated to point to the new GitHub repository.
    
    The setup.cfg file is deleted as it is no longer needed.
    
    The requirements.txt file is updated to pin dependencies for increased stability.
    
    This commit also includes changes to the installation instructions and examples in the README file.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-version = "1.7.0.dev2"
+version = "1.7.1.b0"
 
 # Remember to update local-oldest-requirements.txt when changing the minimum
 # acme/certbot version.
@@ -17,17 +17,19 @@ install_requires = [
     "zope.interface",
 ]
 
-if not os.environ.get("EXCLUDE_CERTBOT_DEPS"):
+if not os.environ.get("SNAP_BUILD"):
     install_requires.extend(
         [
-            "acme>=0.31.0",
-            "certbot>=1.1.0",
+            # We specify the minimum acme and certbot version as the current plugin
+            # version for simplicity. See
+            # https://github.com/certbot/certbot/issues/8761 for more info.
+            f"acme>={version}",
+            f"certbot>={version}",
         ]
     )
 elif "bdist_wheel" in sys.argv[1:]:
     raise RuntimeError(
-        "Unset EXCLUDE_CERTBOT_DEPS when building wheels "
-        "to include certbot dependencies."
+        "Unset SNAP_BUILD when building wheels " "to include certbot dependencies."
     )
 
 setuptools_known_environment_markers = LooseVersion(setuptools_version) >= LooseVersion(
@@ -40,13 +42,17 @@ elif "bdist_wheel" in sys.argv[1:]:
         "Error, you are trying to build certbot wheels using an old version "
         "of setuptools. Version 36.2+ of setuptools is required."
     )
-elif sys.version_info < (3, 3):
-    install_requires.append("mock")
+if os.environ.get("SNAP_BUILD"):
+    install_requires.append("packaging")
 
 docs_extras = [
     "Sphinx>=1.0",  # autodoc_member_order = 'bysource', autodoc_default_flags
     "sphinx_rtd_theme",
 ]
+
+# Load readme to use on PyPI
+with open("README.rst", encoding="utf8") as f:
+    readme = f.read()
 
 
 class PyTest(TestCommand):
@@ -70,11 +76,11 @@ setup(
     name="certbot-dns-beget",
     version=version,
     description="Beget DNS Authenticator plugin for Certbot",
-    url="https://github.com/certbot/certbot",
+    url="https://github.com/skrashevich/certbot-dns-beget",
     author="Certbot Project",
-    author_email="client-dev@letsencrypt.org",
+    author_email="s.krashevich@gmail.com",
     license="Apache License 2.0",
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Plugins",
@@ -82,8 +88,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
